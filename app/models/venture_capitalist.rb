34561845bcb89a0ec0ecda8bf98522bba0f608ad
ytsuji27 <ytsuji91@gmail.com>
2019-02-24 17:40:11 -0800

    Done.

--- app/models/venture_capitalist.rb
+++ app/models/venture_capitalist.rb
@@ -1,3 +1,61 @@
 class VentureCapitalist
 
+  attr_accessor :name, :total_worth
+
+  @@all = []
+
+  def initialize(name,total_worth)
+    @name = name
+    @total_worth = total_worth
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.tres_commas_club
+    @@all.select do |vc|
+      vc.total_worth > 1000000000
+    end
+  end
+
+  def offer_contract(startup,type,amount)
+    FundingRound.new(startup,self,type,amount)
+  end
+
+  def funding_rounds
+    FundingRound.all.select do |fr|
+      fr.venture_capitalist == self
+    end
+  end
+
+  def portfolio
+    self.funding_rounds.map do |fr|
+      fr.startup.name
+    end.uniq
+  end
+
+  def biggest_investment
+    biggest_amount = 0
+    biggest = nil
+    self.funding_rounds.each do |fr|
+      if fr.amount > biggest_amount
+        biggest_amount = fr.amount
+        biggest = fr
+      end
+    end
+    biggest
+  end
+
+  def invested(domain)
+    total = 0
+    self.funding_rounds.each do |fr|
+      if fr.startup.domain == domain
+        total += fr.amount
+      end
+    end
+    total
+  end
+  
 end
--- tools/console.rb
+++ tools/console.rb
@@ -6,6 +6,16 @@ end
 # Insert code here to run before hitting the binding.pry
 # This is a convenient place to define variables and/or set up new object instances,
 # so they will be available to test and play around with in your console
+goggle = Startup.new("Goggle", "Bob", "Search Engine")
+music = Startup.new("Music", "Steve", "Music Streaming")
+
+mm1 = VentureCapitalist.new("Money Man1",1999000)
+mm2 = VentureCapitalist.new("Money Man2",1000000001)
+
+fr1 = FundingRound.new(goggle, mm1, "Angel",500)
+fr2 = FundingRound.new(goggle, mm2, "Pre-Seed",1000)
+fr3 = FundingRound.new(music, mm2, "Series A",10000)
+
 
 binding.pry
 0 #leave this here to ensure binding.pry isn't the last line
