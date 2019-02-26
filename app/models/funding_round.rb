56fd8a09d40433dbb02152b0312278cea60cd651
github username <github email address>
2019-02-25 17:42:56 -0800

    It works

--- app/models/funding_round.rb
+++ app/models/funding_round.rb
@@ -1,3 +1,19 @@
 class FundingRound
+  attr_accessor :startup, :capitalist, :investment, :type
+
+  @@all = []
+
+def initialize(capitalist, startup, investment)
+  @startup = startup
+  @capitalist = capitalist
+  @investment = investment
+  @type = type
+
+  @@all << self
+end
+
+def self.all
+  @@all
+end
 
 end
--- tools/console.rb
+++ tools/console.rb
@@ -3,9 +3,29 @@ require_relative '../config/environment.rb'
 def reload
   load 'config/environment.rb'
 end
-# Insert code here to run before hitting the binding.pry
-# This is a convenient place to define variables and/or set up new object instances,
-# so they will be available to test and play around with in your console
+
+dogs = Startup.new( "Dogs", "Mr. Ruff", "canines")
+cats = Startup.new( "Cats", "Mr. Meow", "felines")
+
+puts dogs.name
+puts cats.founder
+
+jen = VentureCapitalist.new("Jen", 1_000_000_500)
+claire = VentureCapitalist.new("Claire", 1_800_000_000)
+chris = VentureCapitalist.new("Chris", 50)
+
+round1 = FundingRound.new(jen, dogs, 1_000_000)
+round2 = FundingRound.new(chris, dogs, 25)
+round3 = FundingRound.new(claire, cats, 500)
+round4 = FundingRound.new(jen, dogs, 1_000)
+
+
+round1.type = "Angel"
+
+
+
+#dogs.pivot("Squirrels", "woodland creatures")
+
 
 binding.pry
 0 #leave this here to ensure binding.pry isn't the last line
