ca59ca7039bc3d42607de19bc6f42103c8aedef2
kb2shy <chhaianpin@hotmail.com>
2019-02-25 18:53:16 -0800

    Done.

--- app/models/startup.rb
+++ app/models/startup.rb
@@ -1,3 +1,34 @@
 class Startup
+  @@all = []
 
+  attr_reader :founder
+  attr_accessor :name, :domain
+
+  def initialize(name, founder, domain)
+    @name = name
+    @founder = founder
+    @domain = domain
+    @@all << self
+  end
+
+  def pivot(domain, name)
+    @domain = domain
+    @name = name
+  end
+
+  def self.find_by_founder(founder)
+    @@all.find do |company|
+      company.founder == founder
+    end
+  end
+
+  def self.domains
+    @@all.map do |company|
+      company.domain
+    end
+  end
+
+  def self.all
+    @@all
+  end
 end
--- app/models/venture_capitalist.rb
+++ app/models/venture_capitalist.rb
@@ -1,3 +1,22 @@
 class VentureCapitalist
+  @@all = []
+
+  attr_reader :name, :total_worth
+
+  def initialize(name, total_worth)
+    @name = name
+    @total_worth = total_worth
+    @@all << self
+  end
+
+  def self.tres_commas_club
+    @@all.select do |vc|
+      vc.total_worth > 1_000_000_000
+    end
+  end
+
+  def self.all
+    @@all
+  end
 
 end
